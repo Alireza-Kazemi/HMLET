1d561cd116cf8d145de20333a36cb560241703f5
Alireza-Kazemi <62960169+Alireza-Kazemi@users.noreply.github.com>
2022-08-16 13:42:04 -0700

    Update

--- Functions/PermutationTest_MLET.R
+++ Functions/PermutationTest_MLET.R
@@ -1,4 +1,11 @@
 PermutationTest_MLET <- function(data, samples = 2000, paired = T, permuteTrialsWithinSubject = F,   threshold_t = NA){
+  
+  res = ClusterStats_MLET(data, paired = paired, detailed = T, threshold_t = threshold_t)
+  clusterInf = res[[1]]
+  tValues = res[[2]]
+  
+  if(nrow(clusterInf)<1) {stop("No Cluster is found based on this the current threshold")}
+  
   if(is.na(threshold_t)){
     # Treshold should be based on the permutation sampling
     num_sub = length(unique(data$ID))  
@@ -6,24 +13,22 @@ PermutationTest_MLET <- function(data, samples = 2000, paired = T, permuteTrials
   }
   print("Creating unique permutation labels:")
   if(permuteTrialsWithinSubject){
-    tValueDist = TrialLevelPermutationTestWithin_MLET(data, samples = samples, paired = paired, threshold_t = threshold_t)
+    tValueDist = TrialLevelPermutationTestWithin_MLET(data, clusterInf = clusterInf, samples = samples, paired = paired, threshold_t = threshold_t)
     
   }else{
-    tValueDist = SubjectLevelPermutationTestWithin_MLET(data, samples = samples, paired = paired, threshold_t = threshold_t)
+    tValueDist = SubjectLevelPermutationTestWithin_MLET(data, clusterInf = clusterInf, samples = samples, paired = paired, threshold_t = threshold_t)
   }
   
-  res = ClusterStats_MLET(data, paired = paired, detailed = T, threshold_t = threshold_t)
-  sdat = res[[1]]
-  tValues = res[[2]]
+  
 	
-  sdat$pValue = unique(NA)
-  for(i in 1:nrow(sdat)){
-    if(sdat$tStatistic[i]>=0){
-      sdat$pValue[i]=mean(as.numeric(tValueDist$NullDist>sdat$tStatistic[i]))
+  clusterInf$pValue = unique(NA)
+  for(i in 1:nrow(clusterInf)){
+    if(clusterInf$tStatistic[i]>=0){
+      clusterInf$pValue[i]=mean(as.numeric(tValueDist$NullDist>clusterInf$tStatistic[i]))
     }else{
-      sdat$pValue[i]=mean(as.numeric(tValueDist$NullDist<sdat$tStatistic[i]))
+      clusterInf$pValue[i]=mean(as.numeric(tValueDist$NullDist<clusterInf$tStatistic[i]))
     }
   }
   
-  return(list(sdat, tValueDist, tValues, samples))
+  return(list(clusterInf, tValueDist, tValues, samples))
 }
