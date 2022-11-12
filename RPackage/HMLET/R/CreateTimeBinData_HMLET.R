c490368cc2c072cb250f760e8f44c070493b73a9
grishaab <gbandodkar@ucdavis.edu>
2022-11-12 12:19:53 -0800

    updates on PermutationTest, PermutationTestDataPrep, PlotTermporalGazeTrends

--- RPackage/HMLET/R/CreateTimeBinData_HMLET.R
+++ RPackage/HMLET/R/CreateTimeBinData_HMLET.R
@@ -1,21 +1,18 @@
 #' Collapse time points in specified time bins.
 #'
-#' @param data dataframe containing temporal data
-#' @param groupingColumns optional array of strings for grouping variables, defaults to NULL
-#' @param timeBinWidth optional integer specified time interval for time bins, defaults to 250
-#' @param timeMax optional integer, defaults to 3000
-#' @param FixatedOn string for column name of specific AOI in dataframe to be fixated on
-#' @param timepoint optional string representing the name of the column for timepoints, defaults to "timepoint"
-#' @param AOIs optional <DATAYPE?> for areas of interest, defaults to NULL
+#' @param data dataframe containing temporal data.
+#' @param groupingColumns optional array of strings for column names of variables to be grouped, defaults to NULL.
+#' @param timeBinWidth optional integer specified time interval for time bins, defaults to 250.
+#' @param timeMax optional integer for maximum timepoint, defaults to 3000.
+#' @param FixatedOn string for column name of specific AOI in dataframe to be fixated on.
+#' @param timepoint optional string representing column name for timepoints, defaults to "timepoint".
+#' @param AOIs optional array of strings representing column names for areas of interest, defaults to NULL.
 #' @param timeForward optional boolean to sort timebins, defaults to True for ascending order.
-#' @param aggregateFun optional function for aggregation, defaults to mean
+#' @param aggregateFun optional function for aggregation, defaults to mean.
 #'
-#' @return temporal data with sorted time points into time bins
+#' @return temporal data with sorted time points into time bins.
 #'
 #' @export
-
-# explanation for factor function
-# what is an AOI?
 
 CreateTimeBinData_HMLET<- function(data, groupingColumns = NULL, timeBinWidth =  250, timeMax = 3000, FixatedOn,
                              timepoint = "timepoint", AOIs = NULL , timeForward = T, aggregateFun = mean){
@@ -32,8 +29,7 @@ CreateTimeBinData_HMLET<- function(data, groupingColumns = NULL, timeBinWidth = 
 
   }
 
-  # applies the aggregating function on binary AOIs within time bins per each level of the grouping variables defined by the user.
-  # QUESTION: what is happening here and what does the factor function do?
+  # applies aggregating function on binary AOIs within time bins per each level of grouping variables in specified order
   data = data[data[, timepoint]<timeMax, ]
   data$timeBin = ceiling(data[, timepoint]/timeBinWidth-1)*timeBinWidth+timeBinWidth/2
   timeBinsOrder = unique(data$timeBin)
@@ -49,6 +45,7 @@ CreateTimeBinData_HMLET<- function(data, groupingColumns = NULL, timeBinWidth = 
 
   data$timeBinIndex = as.numeric(factor(data$timeBin, levels = levels(data$timeBin), labels = 1:length(levels(data$timeBin))))
 
+  # if AOIs are not specified then create an array of AOIs based on unique values from FixatedOn column
   if(is.null(AOIs)){
     AOIs = unique(data[,c(FixatedOn)])
     AOIs = AOIs[!is.na(AOIs)]
--- RPackage/HMLET/R/PermutationTestDataPrep_HMLET.R
+++ RPackage/HMLET/R/PermutationTestDataPrep_HMLET.R
@@ -1,13 +1,23 @@
 #' Return a data frame compatible for permutation tests routine
 #'
 #'
-#' @param testName Name of this data -- can be used as condition name or test names to compare permutation test results between different tests/conditions later
+#' @param data data frame with temporal data for permutation tests routine.
+#' @param ID optional string for column name that represents IDs within data frame, defaults to "ID"
+#' @param trial string for column name that represents trials within data frame, for example "TrialNum"
+#' @param timepoint string for column name that represents time intervals, for example "timeStamp"
+#' @param condition string for column name that specifies a condition within the data frame
+#' @param gazeInAOI string for column name for specific AOI in dataframe. # TODO: CONFIRM
+#' @param conditionLevels optional array of string column names to denote all the values of conditions, defaults to NULL
+#' @param targetAOI optional string for column name to focus on specific AOI, defaults to NULL
+#' @param testName optional string for name of data -- used as condition name or test names to compare permutation test results between different tests/conditions later, defaults to NULL
 #' @import dplyr
 #' @import miceadds
 #' @import purrr
 #' @import rray
 #' @import tidyr
 #' @import combinat
+#'
+#' @return data frame compatible for permutation tests and other HMLET modules
 #' @export
 PermutationTestDataPrep_HMLET <- function(data, ID = "ID", trial, timepoint, condition, gazeInAOI,
                                   conditionLevels = NULL, targetAOI = NULL, testName = NULL){
