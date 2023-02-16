5252550d64747c5ce5b379a9714cd5062f6d04a3
Alireza-Kazemi <62960169+Alireza-Kazemi@users.noreply.github.com>
2023-02-15 17:55:48 -0800

    LabPC

--- Demo/HMLET_TestingRoutine.R
+++ Demo/HMLET_TestingRoutine.R
@@ -2,6 +2,7 @@ library(HMLET)
 
 
 RD = "C:\\Users\\kazemi\\Documents\\GitHub\\HMLET\\Demo\\"
+WR = "C:\\Users\\kazemi\\Documents\\GitHub\\HMLET\\Demo\\"
 dat = read.table(paste(RD,"HMLET_Testing_Data.csv",sep = ""), header=TRUE, sep=",", strip.white = TRUE)
 
 
@@ -22,8 +23,16 @@ P = PlotTemporalGazeTrends_HMLET(datP, clusterData = Res)
 plot(P)
 
 set.seed(5)
-res = PermutationTest_HMLET(datP, samples = 100, paired = T, permuteTrialsWithinSubject = F)
-res[[1]]
+res = PermutationTest_HMLET(datP, samples = 1000, paired = T, permuteTrialsWithinSubject = F)
+
+write.csv(res[[1]],paste(RD,"ClusterInfos.csv",sep = ""),row.names = F)
+write.csv(res[[2]],paste(RD,"NullDistribution.csv",sep = ""),row.names = F)
+write.csv(res[[3]],paste(RD,"NullDistribution.csv",sep = ""),row.names = F)
+write.table(res[[4]], paste(RD,"PermuationSampleNumber.txt",sep = ""), append = FALSE, sep = " ", dec = ".",
+            row.names = F, col.names = F)
+write.csv(res[[5]],paste(RD,"Data.csv",sep = ""),row.names = F)
+
+
 PlotNullDistribution_HMLET(res)
 PlotTemporalGazeTrends_HMLET(res)
 PlotTemporalGazeTrends_HMLET(res, onlySignificantClusters = F)
