7d1725146ad6c13849a1245c9d00dd015046885f
Alireza Kazemi <alireza.kzmi@gmail.com>
2023-07-31 02:48:28 -0700

    Update RemoveIncompleteTimePoints_HMLET.R

--- RPackage/HMLET/R/RemoveIncompleteTimePoints_HMLET.R
+++ RPackage/HMLET/R/RemoveIncompleteTimePoints_HMLET.R
@@ -5,16 +5,16 @@
 #'
 #' @export
 RemoveIncompleteTimePoints_HMLET <- function(data){
-  rmIdx = as.data.frame(dplyr::summarise(dplyr::group_by(data,ID,timepoint,condition), prop = mean(AOI, na.rm=T)))
-  rmIdx = reshape2::dcast(rmIdx, ID+timepoint~condition, value.var = "prop")
-  rmIdx = rmIdx[!complete.cases(rmIdx),]
-  if(nrow(rmIdx)>0){
-    rmIdx = paste(rmIdx$ID,rmIdx$timepoint,sep = "_")
-    dIdx = paste(data$ID,data$timepoint,sep = "_")
-    data = data[!(dIdx %in% rmIdx),]
-    warning(paste("\n    >> In test data: ",unique(data$testName),
-                  "\n    >> Time points with missing conditions removed! (N =",
-                  length(dIdx[dIdx %in% rmIdx]),")", sep = ""))
-  }
-  return(data)
+	rmIdx = as.data.frame(dplyr::summarise(dplyr::group_by(data,ID,timepoint,condition,testName), prop = mean(AOI, na.rm=T)))
+	rmIdx = reshape2::dcast(rmIdx, ID+timepoint+testName~condition, value.var = "prop")
+	rmIdx = rmIdx[!complete.cases(rmIdx),]
+	if(nrow(rmIdx)>0){
+	  rmIdx = paste(rmIdx$ID,rmIdx$timepoint,rmIdx$testName,sep = "_")
+	  dIdx = paste(data$ID,data$timepoint,data$testName,sep = "_")
+	  data = data[!(dIdx %in% rmIdx),]
+	  warning(paste("\n    >> In test data: ",unique(data$testName),
+					"\n    >> Time points with missing conditions removed! (N =",
+					length(dIdx[dIdx %in% rmIdx]),")", sep = ""))
+	}
+	return(data)
 }
