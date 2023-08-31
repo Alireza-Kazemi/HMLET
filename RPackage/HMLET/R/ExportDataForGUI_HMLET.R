b1d9efa385c44ad12d8bbcc7e298a45224d8c334
Alireza-Kazemi <62960169+Alireza-Kazemi@users.noreply.github.com>
2023-08-30 18:11:36 -0700

    Update ExportDataForGUI_HMLET.R

--- RPackage/HMLET/R/ExportDataForGUI_HMLET.R
+++ RPackage/HMLET/R/ExportDataForGUI_HMLET.R
@@ -53,6 +53,5 @@ ExportDataForGUI_HMLET <- function(data, ID = "ID", trial = "trial", timepoint =
 
 
   write.csv(data, paste(path, fileName, sep = .Platform$file.sep), row.names = F)
-  print(paste("Data is saved in",getwd(),.Platform$file.sep,fileName, sep=""))
-  return(data)
+  print(paste("Data is saved in",path,.Platform$file.sep,fileName, sep=""))
 }
